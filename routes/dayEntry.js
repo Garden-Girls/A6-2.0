3d696ef021016fbaa6ae0ab918e727963135c94e
Jennifer Qi <jeqi@ucsd.edu>
2021-02-25 05:32:58 -0800

    back button

--- routes/dayEntry.js
+++ routes/dayEntry.js
@@ -6,7 +6,70 @@
   res.render('dayEntry');
 };*/
 
-exports.view = function(req, res){
+/*exports.view = function(req, res) {
+	res.render('dayEntry', {
+		"Froggy": [
+		{
+			"date": "02/21/2021",
+			"mood": "red",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Edges of leaves are brown."
+		},
+		{
+			"date": "02/22/2021",
+			"mood": "red",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Some leaves fell off."
+		},
+		{
+			"date": "02/23/2021",
+			"mood": "yellow",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Yellow parts are staying the same size."
+		}
+		],
+		"Emily": [
+		{
+			"date": "02/21/2021",
+			"mood": "red",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Edges of leaves are brown."
+		},
+		{
+			"date": "02/22/2021",
+			"mood": "red",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Some leaves fell off."
+		},
+		{
+			"date": "02/23/2021",
+			"mood": "yellow",
+			"image": [
+				"lorempixel.people.1.jpeg",
+				"img2"
+			],
+			"text": "Yellow parts are staying the same size."
+		}
+		]
+	});
+};*/
+
+exports.view = function(req, res) {
 	var plantToShow = req.params.plantName;
 
 	var monthToShow = req.params.monthNum;
